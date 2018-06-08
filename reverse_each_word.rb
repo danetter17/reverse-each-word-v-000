d5fedce45f19de43b9a17178aceff49f42edab89
Daniel Etter <etter.dt@gmail.com>
2018-06-08 20:02:21 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence1)
+  reverse_sentence=[]
+  split_sentence=sentence1.split
+  split_sentence.each do |word|
+    reverse_sentence << word.reverse
+  end
+  reverse_sentence.join(" ")
+end
+
+def reverse_each_word(sentence1)
+  split_sentence=sentence1.split
+  split_sentence.collect do |word|
+    word.reverse
+  end
+end
